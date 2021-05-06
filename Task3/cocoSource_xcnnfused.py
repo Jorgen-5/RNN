e3ffdc65b20e7d576cee33c771428a9212adcf3c
Hege Russnes <hegerussnes@heges-air.lan>
2021-05-06 15:59:12 +0200

    Debugging

--- Task3/cocoSource_xcnnfused.py
+++ Task3/cocoSource_xcnnfused.py
@@ -501,7 +501,7 @@ class LSTMCell(nn.Module):
         hidden_state_update = torch.mul(output_gate, memory_cell_tanh)
         state_new = torch.cat((hidden_state_update, memory_cell_tanh), dim=1)
         print("state_new:  ", state_new.shape)
-
+        print("")
         return state_new
 
 
