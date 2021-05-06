ea0435fe2349160272bac6256b1c5d4aa5a12811
Hege Russnes <hegerussnes@heges-air.lan>
2021-05-06 14:45:48 +0200

    updated config

--- Task3/cocoSource_xcnnfused.py
+++ Task3/cocoSource_xcnnfused.py
@@ -240,7 +240,6 @@ class RNN(nn.Module):
             # see the simplified rnn for the one layer version
 
             lvl0input = torch.cat((baseimgfeat, tokens_vector), dim=1)
-            print(self.cells)
             updatedstate[0, :] = self.cells[0](lvl0input, current_state[0, :, :])
 
             for layer in range(self.num_rnn_layers - 1):
