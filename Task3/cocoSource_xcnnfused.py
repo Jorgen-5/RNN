a88b5fafa6b7c61f93a1bd26e5434d4fb1710629
Hege Russnes <hegerussnes@heges-air.lan>
2021-05-06 15:10:34 +0200

    updated config

--- Task3/cocoSource_xcnnfused.py
+++ Task3/cocoSource_xcnnfused.py
@@ -481,6 +481,7 @@ class LSTMCell(nn.Module):
 
         print("forget:    ", forget_gate.shape)
         print("state_old: ", state_old[:,self.hidden_state_size:].shape)
+        print("state_old_orig: ", state_old.shape )
 
         memory_cell = torch.mul(forget_gate, state_old[:,self.hidden_state_size:]) #+ torch.mul(input_gate, candidate_memory)
         memory_cell_tanh = torch.tanh(memory_cell)
