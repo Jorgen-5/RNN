d19343939dc496c9b3f63a217eb6c18a72e05b53
Hege Russnes <hegerussnes@heges-air.lan>
2021-05-07 19:59:27 +0200

    Debugging

--- Task4/cocoSource_xcnnfused.py
+++ Task4/cocoSource_xcnnfused.py
@@ -528,10 +528,11 @@ class LSTMCell(nn.Module):
         print("state_old_orig: ", state_old.shape)
         print("candidate_mem_tanh: ", candidate_mem_tanh.shape)
 
+        double_hidden = self.hidden_state_size + self.hidden_state_size
 
-        print("This state: ", state_old[:,self.hidden_state_size:self.hidden_state_size+self.hidden_state_size].shape)
+        print("This state: ", state_old[:,self.hidden_state_size:].shape)
 
-        memory_cell = torch.mul(forget_gate, state_old[:,self.hidden_state_size:]) + torch.mul(input_gate, candidate_mem_tanh)
+        memory_cell = torch.mul(forget_gate, state_old[:,self.hidden_state_size:double_hidden]) + torch.mul(input_gate, candidate_mem_tanh)
 
         memory_cell_tanh = torch.tanh(memory_cell.clone())
 
