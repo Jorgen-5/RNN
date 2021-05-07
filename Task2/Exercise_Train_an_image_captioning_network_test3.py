d9716361c10252962b5638371ddfc93571ce8934
Hege Russnes <hegerussnes@eduroam-193-157-175-63.wlan.uio.no>
2021-05-07 12:35:35 +0200

    Debugging

--- Task2/Exercise_Train_an_image_captioning_network_test3.py
+++ Task2/Exercise_Train_an_image_captioning_network_test3.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
         'modelsDir': 'storedModels_test/',
         'modelName': 'model_0/',  # name of your trained model
         'restoreModelLast': 1,
-        'restoreModelBest': 0,
+        'restoreModelBest': 1,
         'modeSetups': [['train', True], ['val', True]],
         'inNotebook': False,  # If running script in jupyter notebook
         'inference': True
