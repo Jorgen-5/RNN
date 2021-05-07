597cd86c925c95a75aa5aac334b28681dcb46bc5
Hege Russnes <hegerussnes@eduroam-193-157-175-63.wlan.uio.no>
2021-05-07 17:59:07 +0200

    Debugging

--- Task4/cocoSource_xcnnfused.py
+++ Task4/cocoSource_xcnnfused.py
@@ -27,7 +27,7 @@ class imageCaptionModel(nn.Module):
         self.hidden_state_sizes = config['hidden_state_sizes']
         self.num_rnn_layers = config['num_rnn_layers']
         self.cell_type = config['cellType']
-        self.last_layer_size = 10 + config['hidden_state_sizes']
+        self.last_layer_size = 10 + config['hidden_state_sizes'] + self.embedding_size
 
         self.Embedding = nn.Embedding(self.vocabulary_size, self.embedding_size)
 
