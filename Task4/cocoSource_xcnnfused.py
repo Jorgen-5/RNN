8a1723a3e1f02ee688e4e76c85e5aa6770eafd06
Hege Russnes <hegerussnes@eduroam-193-157-175-63.wlan.uio.no>
2021-05-07 17:22:34 +0200

    Debugging

--- Task4/cocoSource_xcnnfused.py
+++ Task4/cocoSource_xcnnfused.py
@@ -91,7 +91,7 @@ class imageCaptionModel(nn.Module):
         print("cnn shape: ", cnn_features.shape)
 
 
-        imgfeat_processed = torch.squeeze(self.inputlayer(cnn_features.transpose(1,2)),3)
+        imgfeat_processed = torch.squeeze(self.inputlayer(cnn_features.transpose(1,2)),2)
 
 
         if current_hidden_state is None:
