4366104bc62f4a7fe02e8e82c4bb87e91e07db98
Hege Russnes <hegerussnes@eduroam-193-157-175-63.wlan.uio.no>
2021-05-07 17:14:54 +0200

    Debugging

--- Task4/cocoSource_xcnnfused.py
+++ Task4/cocoSource_xcnnfused.py
@@ -91,7 +91,7 @@ class imageCaptionModel(nn.Module):
         print("cnn shape: ", cnn_features.shape)
 
 
-        imgfeat_processed = self.inputlayer(cnn_features)
+        imgfeat_processed = self.inputlayer(cnn_features.transpose(1,2))
 
 
         if current_hidden_state is None:
