ea2d62f110bdc7c047372158413ec598395ab68b
Hege Russnes <jrussnes@gmail.com>
2021-05-09 13:14:31 +0200

    Task 4

--- Task3/Exercise_Train_an_image_captioning_network_test3.py
+++ Task3/Exercise_Train_an_image_captioning_network_test3.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
         'restoreModelBest': 0,
         'modeSetups': [['train', True], ['val', True]],
         'inNotebook': False,  # If running script in jupyter notebook
-        'inference': True
+        'inference': False
     }
 
     config = {
