23412d996be95ef29be880b8524546400b4532f1
Hege Russnes <jrussnes@gmail.com>
2021-05-09 13:36:14 +0200

    Task 4

--- Task3/cocoSource_xcnnfused.py
+++ Task3/cocoSource_xcnnfused.py
@@ -251,6 +251,7 @@ class RNN(nn.Module):
             lvl0input = torch.cat((baseimgfeat, tokens_vector), dim=1)
             #print("Current shape: ", current_state.shape)
             updatedstate[0, :] = self.cells[0](lvl0input, current_state[0, :, :])
+            print("Update shape: ", updatedstate.shape)
 
             for layer in range(1,self.num_rnn_layers):
                 #print("layer: ",layer)
